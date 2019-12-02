3173e1ceef5b8b3b48976a7b75ebb801cc01b4e6
Krishna <krishnamurugan589@gmail.com>
2019-12-02 21:22:57 +0530

    (modified.) implemented pass_by_value and pass_by_reference.

--- structures.go
+++ structures.go
@@ -8,6 +8,24 @@ type Saiyan struct{
 	Power int
 }
 
+// creating a Rectangle Type Structure
+type Rectangle struct{
+	length, width int
+}
+
+func (r Rectangle) Area() int{ // Area by Value
+	return r.length * r.width
+}
+
+// adding a Perimeter method to the Rectangle Type
+func (r Rectangle) Perimeter() int{
+	return 2 * (r.length + r.width)
+}
+
+func (r *Rectangle) Area_by_reference() int { // Area by Reference
+	return r.length * r.width
+}
+
 func main(){
 	// inferring Saiyan structure to goku variable
 	goku := Saiyan{"Goku", 9000}
@@ -17,6 +35,14 @@ func main(){
 	// if you really want to change the value, we need to use pointers.
 	SuperOne(&goku)
 	fmt.Print(goku)
+	goku.Hello()
+	fmt.Println(goku)
+	// Instance for Rectangle Type
+	r1 := Rectangle{4, 3}
+	fmt.Println("Rectangle is: ", r1)
+	fmt.Println("Rectangle Area is", r1.Area())
+	fmt.Println("Rectangle Area by Reference: ", (&r1).Area())
+	fmt.Println("Rectangle Perimeter is: ", r1.Perimeter())
 }
 
 func Super(s Saiyan){
@@ -26,3 +52,9 @@ func Super(s Saiyan){
 func SuperOne(s *Saiyan){
 	s.Power += 1000
 }
+
+// functions on structures
+func (s *Saiyan) Hello(){
+	s.Power += 100
+}
+
