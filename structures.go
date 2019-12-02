a9674988f5ede8383f74385cd79a6140fa73ec44
Krishna <krishnamurugan589@gmail.com>
2019-12-02 20:55:43 +0530

    (modified.) added pointer implementation to a structure.

--- structures.go
+++ structures.go
@@ -11,5 +11,18 @@ type Saiyan struct{
 func main(){
 	// inferring Saiyan structure to goku variable
 	goku := Saiyan{"Goku", 9000}
+	Super(goku) // here we are calling the super function to check whether it changes the value of power.
+	// but it wont change the value
+	// because Go passes values to a function as copies.
+	// if you really want to change the value, we need to use pointers.
+	SuperOne(&goku)
 	fmt.Print(goku)
+}
+
+func Super(s Saiyan){
+	s.Power += 1000
+}
+
+func SuperOne(s *Saiyan){
+	s.Power += 1000
 }
