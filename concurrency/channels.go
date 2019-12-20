05a1e3f5ebaedef521a984cec339b6e74397999f
Krishna Murugan <krishnamurugan589@gmail.com>
2019-12-20 16:48:21 +0530

    (added.) channels.

---
+++ concurrency/channels.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+/*
+	Channels.
+	Channels can be thought as pipes using which Goroutines communicate. Similar to how water flows
+	from one end to another in a pipe, data can be sent from one end and received from the another end
+	using channels.
+*/
+
+
+func main()  {
+	
+}
