ace5fbceb9557cbf7827e921178f14cf598c7cb8
Krishna Murugan <krishnamurugan589@gmail.com>
2019-12-28 11:47:54 +0530

    (modified.) custom errors and added getting more info about the error using struct methods.

--- custom-errors.go
+++ custom-errors.go
@@ -27,6 +27,50 @@ func circleArea(radius float64) (float64, error) {
 	return math.Pi * radius * radius, nil
 }
 
+/* Providing more information about the error using methods on struct types */
+
+type rectAreaError struct {
+	err string
+	length float64
+	width float64
+}
+
+// Now we have the error type, now lets implement the error interface and add a couple of methods on
+// the error type to provide more info. about the error.
+
+func (e *rectAreaError) Error() string {
+	return e.err
+}
+
+func (e *rectAreaError) lengthNegative() bool {
+	return e.length < 0
+}
+
+func (e *rectAreaError) widthNegative() bool {
+	return e.width < 0
+}
+
+// The next step is to write the area calculation function.
+func calculateRectArea(length, width float64) (float64, error) {
+	err := ""
+	if length < 0 {
+		err += "Length is less than zero"
+	}
+	if width < 0 {
+		if err == ""{
+			err = "Width is less than zero"
+		} else {
+			err += ", Width is less than zero"
+		}
+	}
+	if err != "" {
+		return 0, &rectAreaError{err, length, width}
+	}
+	return length * width, nil
+}
+
+/* End*/
+
 func main()  {
 
 	
@@ -55,13 +99,13 @@ func main()  {
 		radius is negative.
 	*/
 
-	radius := -20.1
-	/*area, err := circleArea(radius)
+	radius := 20.1
+	area1, err := circleArea(radius)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("The area of circle is %.2f\n", area)*/
+	fmt.Printf("The area of circle is %.2f\n", area1)
 
 	/*
 		Adding more information to the error using Errorf.
@@ -93,18 +137,38 @@ func main()  {
 
 		Now lets rewrite the snippet in the main function.
 	*/
-	area, err := circleArea(radius)
+	area2, err := circleArea(radius)
 	if err != nil {
 		if err, ok := err.(*areaError); ok{
-			fmt.Printf("Radius %.2f is less than zero", err.radius)
+			fmt.Printf("Radius %.2f is less than zero\n", err.radius)
 			return
 		}
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("Area of the circle %.2f", area) 
+	fmt.Printf("Area of the circle %.2f\n", area2) 
 
 	/*
 		Providing more information about the error using methods on struct types.
+
+		Now we'll calculate the area of the rectangle.
+
+		The first step is to create a struct to represent the error.
 	*/
+	length, width := 20.4, 19.8
+	rectArea, err := calculateRectArea(length, width)
+	if err != nil {
+		if err, ok := err.(*rectAreaError); ok {
+			if err.lengthNegative() {
+				fmt.Printf("error: %.2f is less than zero\n", err.length)
+			}
+			if err.widthNegative() {
+				fmt.Printf("error: %.2f is less than zero\n", err.width)
+			}
+			return
+		}
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("area of rect", rectArea)
 }
